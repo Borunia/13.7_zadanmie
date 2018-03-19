b2f2b1e174ed7ec4d3d54d48b193785f1404775e
Borunia <borekosp@gmail.com>
2018-03-19 17:29:05 +0100

    add changes after comments

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-
 process.stdin.setEncoding('Utf-8');
 var fs = require('fs');
 var colors = require('colors');
-
-
-/*
-fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-    console.log('Dane przed zapisem!'.blue);
-    console.log(data);
-    fs.appendFile('./tekst.txt','\nA tak wyglądaja po zapisie', function(err) {
-        if (err) throw err; // jeśli pojawi się błąd, wyrzuć wyjątek
-        console.log('Zapisano!'.blue);
-        fs.readFile('./tekst.txt', 'utf-8', function(err,data){
-            console.log('Dane po zapisie'.blue)
-            console.log(data);
-        });
-    });   
-});
-*/
 
 fs.readdir('./','utf-8', function(err, files){
     console.log('Pliki katalogu zadanie_13_7:'.green);
@@ -27,7 +10,3 @@ fs.readdir('./','utf-8', function(err, files){
         console.log('Zapisono w pliku lista.txt'.red)
     })
 });
-
-
-
-
